77eb84b5edaff78d1df6e7d2983170c90978b9e4
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-02 18:53:35 +0800

    spring boot base code

---
+++ gateway-service/src/main/java/com/project/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.project.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(GatewayApplication.class).run(args);
+    }
+
+}
