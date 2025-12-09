725b78c0618fff1836d6966b0a6b444aef0f38d1
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-10 01:12:42 +0800

    add user service base

---
+++ user-service/src/main/java/com/project/user/UserApplication.java
@@ -0,0 +1,23 @@
+package com.project.user;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+//@EntityScan(basePackages = "com.edu.base.domain")
+//@EnableFeignClients
+public class UserApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(UserApplication.class).run(args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
