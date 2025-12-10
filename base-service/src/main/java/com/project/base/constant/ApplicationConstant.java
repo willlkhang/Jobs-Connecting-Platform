52e21467eb6aabf9d5d325ff9b344c8e3128093f
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-10 15:17:44 +0800

    add db chemma

---
+++ base-service/src/main/java/com/project/base/constant/ApplicationConstant.java
@@ -0,0 +1,6 @@
+package com.project.base.constant;
+
+public class ApplicationConstant {
+    public static final int STATUS_OK = 200;
+    public static final int STATUS_ERROR = 201;
+}
---
+++ base-service/src/main/java/com/project/base/domain/User.java
@@ -0,0 +1,16 @@
+package com.project.base.domain;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    
+}
