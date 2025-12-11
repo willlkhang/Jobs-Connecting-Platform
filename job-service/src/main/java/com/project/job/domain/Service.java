9ebff6343aa937c921b996d490a76186718bbcc8
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-11 12:09:15 +0800

    add entity for service. So this means a job-service has 2 tables which are category and service, a service can belong to different category

--- job-service/src/main/java/com/project/job/domain/Service.java
+++ job-service/src/main/java/com/project/job/domain/Service.java
@@ -3,6 +3,9 @@ package com.project.job.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.util.Set;
+
 @Entity
 @Table(name="Service")
 @Data
@@ -17,5 +20,72 @@ public class Service {
     @Column(name = "user_id")
     private Long userId;
 
+    @ManyToMany
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Set<Category> categories;
 
+    @Column(name = "service_name")
+    private String serviceName;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "price")
+    private BigDecimal price;
+    @Column(name = "processed_number") //this mean the number of customer I used this service in the past
+    private Integer processedNumber;
+
+    public Long getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getProcessedNumber() {
+        return processedNumber;
+    }
+
+    public void setProcessedNumber(Integer processedNumber) {
+        this.processedNumber = processedNumber;
+    }
 }
