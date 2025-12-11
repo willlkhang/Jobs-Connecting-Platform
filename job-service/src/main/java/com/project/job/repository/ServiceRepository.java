9ff4a3e25dae8ff752298823f15e62ba4742a794
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-11 14:33:03 +0800

    add in job-service, 1 repo for fetching services by name, and 2 updating repo for increasing and decreasing processed number

--- job-service/src/main/java/com/project/job/repository/ServiceRepository.java
+++ job-service/src/main/java/com/project/job/repository/ServiceRepository.java
@@ -16,4 +16,17 @@ public interface ServiceRepository extends JpaRepository<Service, Integer> {
     @Query("SELECT s FROM Service s JOIN s.categories c WHERE c.categoryId =:categoryId")
     List<Service> getServiceByCategoryId(@Param("category_id") Long categoryId);
 
+    @Query("SELECT s FROM  Service s WHERE s.serviceName =:serviceName")
+    List<Service> getServiceByName(@Param("serviceName") String serviceName);
+
+    //The query which interrupts the data such as update requires these 2 annotations
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Service s SET s.processedNumber = (s.processedNumber + 1) WHERE s.serviceId =:serviceId")
+    void increaseProcessedNumber(@Param("serviceId") Integer serviceId, @Param("processedNumber") Integer processedNumber);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Service s SET s.processedNumber = (s.processedNumber - 1) WHERE s.serviceId =:serviceId")
+    void decreaseProcessedNumber(@Param("serviceId") Integer serviceId, @Param("processedNumber") Integer processedNumber);
 }
