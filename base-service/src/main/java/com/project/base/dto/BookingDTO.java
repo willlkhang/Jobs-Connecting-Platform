168541c4c1614497c535a46fb6507286b19394ed
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-15 17:48:10 +0800

    add bookingDTO to base-service

---
+++ base-service/src/main/java/com/project/base/dto/BookingDTO.java
@@ -0,0 +1,53 @@
+package com.project.base.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class BookingDTO {
+
+    private Long bookingId;
+    private Long userId;
+    private BigDecimal totalAmount;
+    private Integer status;
+    private Date date;
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
