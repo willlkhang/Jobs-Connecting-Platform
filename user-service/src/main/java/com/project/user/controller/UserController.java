317154803e844e80229b0247abe96585671333bc
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-15 22:19:39 +0800

    add allowed method repository

--- user-service/src/main/java/com/project/user/controller/UserController.java
+++ user-service/src/main/java/com/project/user/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.project.user.controller;
 
+import com.project.base.domain.Role;
+import com.project.base.domain.User;
+
+import com.project.user.service.UserService;
+import com.project.user.service.RoleService;
+
+import com.project.user.repository.UserRepository;
+import com.project.user.repository.RoleRepository;
+import com.project.user.repository.AllowedMethodRepository;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class UserController {
 }
--- user-service/src/main/java/com/project/user/repository/AllowedMethodRepository.java
+++ user-service/src/main/java/com/project/user/repository/AllowedMethodRepository.java
@@ -1,4 +1,15 @@
 package com.project.user.repository;
 
-public interface AllowedMethodRepository {
+import com.project.base.domain.AllowedMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AllowedMethodRepository extends JpaRepository<AllowedMethod, Long> {
+
+    @Query("SELECT a FROM AllowedMethod a WHERE a.methodName= :allowedMethod")
+    AllowedMethod checkAllowedMethod(@Param("allowedMethod") String allowedMethod);
+
+    @Query("SELECT r FROM AllowedMethod r WHERE r.methodName= :method")
+    AllowedMethod getRoleByMethod(@Param("method") String method);
 }
