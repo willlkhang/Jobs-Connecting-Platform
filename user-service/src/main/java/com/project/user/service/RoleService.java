09ea158ef81333de9fc9182a56422163adb9fcc3
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-15 22:37:22 +0800

    add roleservice and its implementation

--- user-service/src/main/java/com/project/user/service/RoleService.java
+++ user-service/src/main/java/com/project/user/service/RoleService.java
@@ -1,4 +1,15 @@
 package com.project.user.service;
 
+import com.project.base.domain.Role;
+import com.project.base.exception.RoleException;
+
+import java.util.List;
+
 public interface RoleService {
+
+    void updateRole(String username, Role roleName) throws RoleException;
+
+    void updateAllowedMethod(String username, Role role) throws RoleException;
+
+    List<Role> listOfRoleByUsername(String username);
 }
--- user-service/src/main/java/com/project/user/service/RoleServiceImpl.java
+++ user-service/src/main/java/com/project/user/service/RoleServiceImpl.java
@@ -1,4 +1,48 @@
 package com.project.user.service;
 
-public class RoleServiceImpl {
+import com.project.base.domain.AllowedMethod;
+import com.project.base.domain.Role;
+import com.project.base.domain.User;
+
+import com.project.base.exception.RoleException;
+
+import com.project.user.repository.UserRepository;
+import com.project.user.repository.RoleRepository;
+import com.project.user.repository.AllowedMethodRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AllowedMethodRepository allowedMethodRepository;
+
+    public RoleServiceImpl() {
+        super();
+    }
+
+    @Override
+    public List<Role> listOfRoleByUsername(String username) {
+        return List.of();
+    }
+
+    @Override
+    public void updateAllowedMethod(String username, Role role) throws RoleException {
+
+    }
+
+    @Override
+    public void updateRole(String username, Role roleName) throws RoleException {
+
+    }
 }
