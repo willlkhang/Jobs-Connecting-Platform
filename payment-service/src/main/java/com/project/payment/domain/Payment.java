9931399bde8bdcac229c596900046dde7dc9c099
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-18 15:24:48 +0800

    create payment entity and payment method enum in payment service

---
+++ payment-service/src/main/java/com/project/payment/domain/Payment.java
@@ -0,0 +1,29 @@
+package com.project.payment.domain;
+
+import com.project.payment.enumeration.PaymentMethod;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "Payment")
+@Data
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+    private Long bookingId;
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date paymentDate;
+
+}
---
+++ payment-service/src/main/java/com/project/payment/enumeration/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.project.payment.enumeration;
+
+public enum PaymentMethod {
+    VISA_MASTER, AFTER_PAY, PAY_PAL
+}
