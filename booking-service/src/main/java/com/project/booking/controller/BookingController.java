58f051747a54b5cd8adf2f36ba91a3caf0ac545d
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-30 12:13:58 +0800

    migrate to linux from mac

--- booking-service/src/main/java/com/project/booking/controller/BookingController.java
+++ booking-service/src/main/java/com/project/booking/controller/BookingController.java
@@ -1,4 +1,17 @@
 package com.project.booking.controller;
 
+import com.project.base.constant.ApplicationConstant;
+
+import com.project.base.dto.SolutionDTO;
+import com.project.base.dto.BookingDTO;
+import com.project.base.dto.Result;
+import com.project.base.exception.BusinessException;
+
+import com.project.booking.domain.Booking;
+import com.project.booking.mapper.BookingMapper;
+import com.project.booking.repository.BookingRepository;
+import com.project.booking.service.BookingService;
+
+
 public class BookingController {
 }
---
+++ booking-service/src/main/java/com/project/booking/service/JobService.java
@@ -0,0 +1,13 @@
+//package com.project.booking.service;
+//
+//import com.project.base.dto.SolutionDTO;
+//import com
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//
+//public class JobService {
+//}
