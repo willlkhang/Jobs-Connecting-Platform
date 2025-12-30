1b0d458ce93d81d985b9b48d269c5d097188920e
willlkhang <minhkhangnguyen31104@gmail.com>
2025-12-30 16:43:42 +0800

    complete payment rest controller

--- payment-service/src/main/java/com/project/payment/controller/PaymentController.java
+++ payment-service/src/main/java/com/project/payment/controller/PaymentController.java
@@ -1,4 +1,31 @@
 package com.project.payment.controller;
 
-public record PaymentController() {
+import com.project.payment.domain.Payment;
+import com.project.payment.service.PaymentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @GetMapping("/get-payment-by-id")
+    public  ResponseEntity<?> getPaymentById(@RequestParam Long paymentId){
+        Payment payment = paymentService.getPaymentById(paymentId);
+        if(payment == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(payment);
+    }
+
+    @PostMapping("/save-payment")
+    public void savePayment(@RequestParam Long bookingId, @RequestParam BigDecimal price){
+        paymentService.savePayment(bookingId, price);
+    }
 }
