07b9213eda7d3c7039a843841905ae044a02205d
willlkhang <minhkhangnguyen31104@gmail.com>
2026-01-02 09:18:39 +0800

    config topic payment and, topic payment resutl to payment service

--- payment-service/src/main/java/com/project/payment/config/KafkaConfig.java
+++ payment-service/src/main/java/com/project/payment/config/KafkaConfig.java
@@ -1,4 +1,32 @@
 package com.project.payment.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.topic.producer.payment.name}")
+    private String topicPayment;
+    @Value("${spring.kafka.topic.consumer.payment.name}")
+    private String topicPaymentResult;
+
+    @Bean
+    public NewTopic bookingTopic() {
+        return TopicBuilder.name(topicPayment)
+                .partitions(3)
+                .compact()
+                .build();
+    }
+
+    @Bean
+    public NewTopic bookingResult() {
+        return TopicBuilder.name(topicPaymentResult)
+                .partitions(3)
+                .compact()
+                .build();
+    }
 }
---
+++ payment-service/src/main/java/com/project/payment/consumer/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.project.payment.consumer;
+
+import com.project.base.dto.BookingEvent;
+
+import com.project.payment.service.PaymentService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+}
