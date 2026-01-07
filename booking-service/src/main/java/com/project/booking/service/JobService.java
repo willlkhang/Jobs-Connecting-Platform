5fffb861dc22d2f084c6b4f08c1af7b90bc02d5a
willlkhang <minhkhangnguyen31104@gmail.com>
2026-01-07 21:44:52 +0800

    successfully transfer all api to standard restful api

--- booking-service/src/main/java/com/project/booking/service/JobService.java
+++ booking-service/src/main/java/com/project/booking/service/JobService.java
@@ -3,17 +3,19 @@ package com.project.booking.service;
 import com.project.base.dto.SolutionDTO;
 import com.project.base.exception.BusinessException;
 
+import jakarta.ws.rs.Path;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "job-service")
 public interface JobService {
 
-    @GetMapping("/check-exit-solution-by-id")
-    ResponseEntity<SolutionDTO> checkSolutionExistById(@RequestParam("solutionId") Long id) throws BusinessException;
+    @GetMapping("/solution/{id}")
+    ResponseEntity<SolutionDTO> checkSolutionExistById(@PathVariable Long id) throws BusinessException;
 
-    @GetMapping("/check-exit-solution-by-name")
-    ResponseEntity<SolutionDTO> checkSolutionExistByName(@RequestParam("solutionName") String name) throws BusinessException;
+    @GetMapping("/solution")
+    ResponseEntity<SolutionDTO> checkSolutionExistByName(@RequestParam String name) throws BusinessException;
 }
--- booking-service/src/main/java/com/project/booking/service/UserService.java
+++ booking-service/src/main/java/com/project/booking/service/UserService.java
@@ -6,15 +6,16 @@ import com.project.base.exception.BusinessException;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service")
 public interface UserService {
 
-    @GetMapping("/get-user-by-id")
-    ResponseEntity<User> getUserById(@RequestParam("userId") Long id);
+    @GetMapping("/user/{id}")
+    ResponseEntity<User> getUserById(@PathVariable Long id);
 
-    @GetMapping("/get-user-by-username")
-    ResponseEntity<User> getUserByUserName(@RequestParam("username") String username);
+    @GetMapping("/user")
+    ResponseEntity<User> getUserByUserName(@RequestParam("name") String username);
 
 }
