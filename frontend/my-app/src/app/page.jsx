1bfcbef17c144e1bac5457f5e0cdc253b80dffaa
willlkhang <minhkhangnguyen31104@gmail.com>
2026-01-14 22:44:58 +0800

    test fetching api for front end

---
+++ frontend/my-app/src/app/page.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect, useState } from "react";
+export default function Home() {
+
+  const [count, setCount] = useState(1);
+  const [sum, setSum] = useState(1);
+
+  useEffect(() => {
+    console.log("check ");
+  }, []);
+
+  useEffect(() => {
+    console.log("Check no depend");
+  });
+
+  const getApi = async () => {
+    const result = await fetch("https://jsonplaceholder.typicode.com/users").catch((error) => {
+      console.log()
+  });
+    const data = result.json();
+    console.log("Data: ", data);
+  };
+
+  useEffect(() => {
+    getApi()
+  }, []);
+
+  return (
+    <>
+      <button onClick={() => {
+        setCount(pre => pre + 1)
+      }}>Count {count} </button>
+
+      <br />
+
+      <button onClick={() => {
+        setSum(pre => pre + pre)
+      }}>Sum {sum}</button>
+    </>
+  );
+}
