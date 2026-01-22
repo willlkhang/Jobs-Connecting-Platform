0cddf0c72a6198d334cf0f2c194e8a48ad3253da
willlkhang <minhkhangnguyen31104@gmail.com>
2026-01-22 23:48:08 +0800

    reorganize banner header and content in main page, making them no t overlap

---
+++ booking-service/target/generated-sources/annotations/com/project/booking/mapper/BookingMapperImpl.java
@@ -0,0 +1,32 @@
+package com.project.booking.mapper;
+
+import com.project.base.dto.BookingDTO;
+import com.project.booking.domain.Booking;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-01-21T18:03:18+1030",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.9 (Ubuntu)"
+)
+@Component
+public class BookingMapperImpl implements BookingMapper {
+
+    @Override
+    public BookingDTO bookingDTO(Booking booking) {
+        if ( booking == null ) {
+            return null;
+        }
+
+        BookingDTO bookingDTO = new BookingDTO();
+
+        bookingDTO.setBookingId( booking.getBookingId() );
+        bookingDTO.setUserId( booking.getUserId() );
+        bookingDTO.setTotalAmount( booking.getTotalAmount() );
+        bookingDTO.setStatus( booking.getStatus() );
+        bookingDTO.setBookingDate( booking.getBookingDate() );
+
+        return bookingDTO;
+    }
+}
