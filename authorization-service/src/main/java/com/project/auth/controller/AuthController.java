cc5cbcad93b358df7d83867ebd86f3ab910eeee5
willlkhang <minhkhangnguyen31104@gmail.com>
2026-01-28 13:07:37 +0800

    fix services config to make it dynamic to vps environmtn for authen

--- authorization-service/src/main/java/com/project/auth/controller/AuthController.java
+++ authorization-service/src/main/java/com/project/auth/controller/AuthController.java
@@ -1,18 +1,13 @@
 package com.project.auth.controller;
 
-import com.project.auth.repository.AllowedMethodRepository;
 import com.project.auth.service.AllowedMethodService;
-
-import com.project.base.domain.AllowedMethod;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 public class AuthController {
