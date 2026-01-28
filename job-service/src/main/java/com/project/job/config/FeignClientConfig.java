493f465eb878df5685881b5994eb2a0887fb0e13
willlkhang <minhkhangnguyen31104@gmail.com>
2026-01-28 12:03:31 +0800

    add authorization login back to user-service, also, fixed feign client conflict between user and job serivce

---
+++ job-service/src/main/java/com/project/job/config/FeignClientConfig.java
@@ -0,0 +1,37 @@
+/**
+ * @Author: Will Nguyen (Minh Khang)
+ * @Date: 28/01/2026
+ * @Description: This class is for dealing with conflict between authentication in user-service and being called
+ * by job-service
+ * */
+package com.project.job.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Configuration
+public class FeignClientConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+
+            //get the current incoming request
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+            if (attributes != null) {
+                //extract authorization header (bearer token)
+                String authHeader = attributes.getRequest().getHeader("Authorization");
+
+                //if token exists, attach it to feign request
+                if (authHeader != null) {
+                    requestTemplate.header("Authorization", authHeader);
+                }
+            }
+        };
+    }
+}
--- job-service/src/main/java/com/project/job/service/UserService.java
+++ job-service/src/main/java/com/project/job/service/UserService.java
@@ -1,13 +1,14 @@
 package com.project.job.service;
 
 import com.project.base.domain.User;
+import com.project.job.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user-service", configuration = FeignClientConfig.class)
 public interface UserService {
 
     @GetMapping("/{id}")
